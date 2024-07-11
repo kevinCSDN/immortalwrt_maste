b8113e5025de24c2c19639cca43039fb56ce239c
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-07-11 20:59:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -88,17 +88,17 @@
 #git clone --depth=1 https://github.com/derisamedia/luci-theme-alpha package/luci-theme-alpha
 #git clone --depth=1 https://github.com/animegasan/luci-app-alpha-config package/luci-theme-alpha-config
 
-#克隆passwall环境插件
-#git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
+克隆passwall环境插件
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
 
-#克隆的源码放在small文件夹,预先建立small文件夹
-#mkdir package/small
-#pushd package/small
+克隆的源码放在small文件夹,预先建立small文件夹
+mkdir package/small
+pushd package/small
 
 #克隆源码
 
 #passwall2
-#git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
+git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
 #git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
 #mosdns
 # git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
@@ -109,4 +109,4 @@
 
 # git clone https://github.com/kevinCSDN/luci-app-vssr-plus.git  
 
-#popd
+popd
